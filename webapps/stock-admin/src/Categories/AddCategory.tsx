ab33a9a926afad35e39a6f1992012a27c52f2f18
James Wilson <jw@jameswilson.io>
2022-10-22 12:28:57 +1100

    basic routing works

--- webapps/stock-admin/src/Categories/AddCategory.tsx
+++ webapps/stock-admin/src/Categories/AddCategory.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const AddCategory = () => {
+  const formSubmitted = (e: React.SyntheticEvent): void => {
+    console.log("CLICKED");
+    e.preventDefault();
+  };
   return (
     <div>
       <h2>Add New Category</h2>
+      <Form onSubmit={formSubmitted}>
+        <Form.Group controlId="formCategoryName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control placeholder="Category Name" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 };
