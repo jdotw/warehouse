bdacdfb71152b11e2b86bbecbcbff7ff9b187912
James Wilson <jw@jameswilson.io>
2022-10-23 10:43:36 +1100

    Implemented delete of category with confirmation model

--- webapps/stock-admin/src/Categories/CategoryList.tsx
+++ webapps/stock-admin/src/Categories/CategoryList.tsx
@@ -1,6 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import React, { useEffect, useState } from "react";
-import { Alert, Button, Table } from "react-bootstrap";
+import React, { SyntheticEvent, useEffect, useState } from "react";
+import { Alert, Button, Modal, Table } from "react-bootstrap";
 import Loading from "../components/Loading";
 import AddCategory from "./AddCategory";
 import "./CategoryList.css";
@@ -16,6 +16,9 @@ const CategoryList = () => {
   const [categories, setCategories] = useState<Category[]>();
   const [categoryLoadError, setCategoryLoadError] = useState<Error>();
   const [showAdd, setShowAdd] = useState(false);
+
+  const [categoryToDelete, setCategoryToDelete] = useState<Category>();
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
 
   const loadCategories = async () => {
     const domain = "localhost:8080";
@@ -60,31 +63,96 @@ const CategoryList = () => {
     loadCategories();
   };
 
+  const deleteClicked = (category: Category) => {
+    setCategoryToDelete(category);
+    setShowDeleteConfirmation(true);
+  };
+
+  const deleteCategoryCancelled = () => {
+    setShowDeleteConfirmation(false);
+  };
+
+  const deleteCategoryConfirmed = async () => {
+    setShowDeleteConfirmation(false);
+    if (!categoryToDelete) {
+      console.log("ERROR: categoryToDelete is nil");
+      return;
+    }
+    const domain = "localhost:8080";
+    try {
+      const accessToken = await getAccessTokenSilently({
+        audience: `http://${domain}/`,
+        scope: "write:category",
+      });
+      const deleteCategoryURL = `/api/categories/${categoryToDelete?.id}`;
+      const response = await fetch(deleteCategoryURL, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        method: "DELETE",
+      });
+      const response_json = await response.json();
+      console.log("DELETE JSON: ", response_json);
+      setCategories(categories?.filter((x) => x.id != categoryToDelete.id));
+    } catch (error: any) {
+      console.log("ERROR: ", error.message);
+      alert(error.message);
+    } finally {
+      setCategoryToDelete(undefined);
+    }
+  };
+
   return (
-    <div className="CategoryList">
-      <div className="TopButtonBar">
-        <Button onClick={() => setShowAdd(true)}>Create Category</Button>
-      </div>
-      {showAdd && <AddCategory onAdded={(err?: Error) => onAdded(err)} />}
-      {categoryLoadError ? (
-        <div>Failed to load Categories: {categoryLoadError.message}</div>
-      ) : (
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Name</th>
-            </tr>
-          </thead>
-          <tbody>
-            {categories!.map((c) => (
-              <tr key={c.id}>
-                <td>{c.name}</td>
+    <>
+      <div className="CategoryList">
+        <div className="TopButtonBar">
+          <Button onClick={() => setShowAdd(true)}>Create Category</Button>
+        </div>
+        {showAdd && <AddCategory onAdded={(err?: Error) => onAdded(err)} />}
+        {categoryLoadError ? (
+          <div>Failed to load Categories: {categoryLoadError.message}</div>
+        ) : (
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Name</th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
-      )}
-    </div>
+            </thead>
+            <tbody>
+              {categories!.map((c) => (
+                <tr key={c.id}>
+                  <td>
+                    <div className="CategoryRowContainer">
+                      <div className="CategoryName">{c.name}</div>
+                      <Button variant="danger" onClick={() => deleteClicked(c)}>
+                        Delete
+                      </Button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
+      </div>
+      <Modal show={showDeleteConfirmation} onHide={deleteCategoryCancelled}>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete Category '{categoryToDelete?.name}'</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Are you sure you want to delete the category named '
+          {categoryToDelete?.name}'
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={deleteCategoryCancelled}>
+            Cancel
+          </Button>
+          <Button variant="primary" onClick={deleteCategoryConfirmed}>
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
