fcecaebdcd7062bf8cc2d7fab98f5179bea37d5d
James Wilson <jw@jameswilson.io>
2022-10-23 12:41:58 +1100

    Added AddItem

---
+++ webapps/stock-admin/src/Categories/AddItem.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { useAuth0 } from "@auth0/auth0-react";
+import "./AddItem.css";
+
+type Props = {
+  categoryID: string;
+  onAdded: (error?: Error) => void;
+};
+
+const AddItem = (props: Props) => {
+  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const [name, setName] = useState("");
+  const { categoryID, onAdded } = props;
+
+  const formSubmitted = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    console.log("CREATING: ", name);
+    const domain = "localhost:8080";
+    try {
+      const accessToken = await getAccessTokenSilently({
+        audience: `http://${domain}/`,
+        scope: "write:item",
+      });
+      console.log("TOKEN: ", accessToken);
+      const response = await fetch(`/api/categories/${categoryID}/items`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({
+          name: name,
+        }),
+        method: "POST",
+      });
+      const response_json = await response.json();
+      console.log("JSON: ", response_json);
+      if (onAdded) onAdded();
+    } catch (error: any) {
+      console.log("ERROR: ", error.message);
+      if (onAdded) onAdded(error);
+    }
+  };
+
+  return (
+    <div className="Container">
+      <h2>Add New Item</h2>
+      <Form onSubmit={formSubmitted}>
+        <Form.Group controlId="formItemName" className="FieldRow">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            placeholder="Item Name"
+            onChange={(e) => setName(e.currentTarget.value)}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default AddItem;
