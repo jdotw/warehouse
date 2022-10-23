af4c5a80524d38a420751935e5aea9d539152df3
James Wilson <jw@jameswilson.io>
2022-10-24 07:33:30 +1100

    Implemented Delete Item

--- webapps/stock-admin/src/Categories/Category.tsx
+++ webapps/stock-admin/src/Categories/Category.tsx
@@ -1,6 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React, { useEffect, useState } from "react";
-import { Button, Table } from "react-bootstrap";
+import { Button, Modal, Table } from "react-bootstrap";
 import { Link, useParams } from "react-router-dom";
 import Loading from "../components/Loading";
 import AddItem from "./AddItem";
@@ -26,6 +26,8 @@ const Category = () => {
   const [loadingItems, setLoadingItems] = useState(true);
   const [showAdd, setShowAdd] = useState(false);
   const [itemLoadError, setItemLoadError] = useState<Error>();
+  const [itemToDelete, setItemToDelete] = useState<Category>();
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
 
   const loadCategory = async (categoryID: string) => {
     const domain = "localhost:8080";
@@ -43,10 +45,12 @@ const Category = () => {
       const response_json = await response.json();
       console.log("JSON: ", response_json);
       setCategory(response_json);
+      setItemLoadError(undefined);
       setLoadingCategory(false);
     } catch (error: any) {
       console.log("ERROR: ", error.message);
       setCategory(undefined);
+      setItemLoadError(error);
       setLoadingCategory(false);
     }
   };
@@ -87,53 +91,110 @@ const Category = () => {
     loadItems(categoryID!);
   };
 
+  const deleteClicked = (category: Category) => {
+    setItemToDelete(category);
+    setShowDeleteConfirmation(true);
+  };
+
+  const deleteItemCancelled = () => {
+    setItemToDelete(undefined);
+    setShowDeleteConfirmation(false);
+  };
+
+  const deleteItemConfirmed = async () => {
+    setShowDeleteConfirmation(false);
+    if (!itemToDelete) {
+      console.log("ERROR: itemToDelete is nil");
+      return;
+    }
+    const domain = "localhost:8080";
+    try {
+      const accessToken = await getAccessTokenSilently({
+        audience: `http://${domain}/`,
+        scope: "write:category",
+      });
+      const response = await fetch(`/api/items/${itemToDelete?.id}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        method: "DELETE",
+      });
+      const response_json = await response.json();
+      console.log("DELETE JSON: ", response_json);
+      setItems(items?.filter((x) => x.id != itemToDelete.id));
+    } catch (error: any) {
+      console.log("ERROR: ", error.message);
+      alert(error.message);
+    } finally {
+      setItemToDelete(undefined);
+    }
+  };
+
   if (loadingCategory || loadingItems) return <Loading />;
 
   if (!category) return <div>Category not found</div>;
 
   return (
-    <div>
-      <div className="HeaderContainer">
-        <h1>{category.name}</h1>
-        <div className="TopButtonBar">
-          <Button onClick={() => setShowAdd(true)}>Add Item</Button>
+    <>
+      <div>
+        <div className="HeaderContainer">
+          <h1>{category.name}</h1>
+          <div className="TopButtonBar">
+            <Button onClick={() => setShowAdd(true)}>Add Item</Button>
+          </div>
         </div>
-      </div>
-      {showAdd && (
-        <AddItem
-          categoryID={category.id}
-          onAdded={(err?: Error) => onAdded(err)}
-          onCancelled={() => setShowAdd(false)}
-        />
-      )}
-      {itemLoadError ? (
-        <div>Failed to load Items: {itemLoadError.message}</div>
-      ) : (
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Name</th>
-            </tr>
-          </thead>
-          <tbody>
-            {items!.map((i) => (
-              <tr key={i.id}>
-                <td>
-                  <div className="ItemRowContainer">
-                    <div className="ItemName">
-                      <Link to={"items/" + i.id}>{i.name}</Link>
-                    </div>
-                    <Button variant="danger" onClick={() => deleteClicked(c)}>
-                      Delete
-                    </Button>
-                  </div>
-                </td>
+        {showAdd && (
+          <AddItem
+            categoryID={category.id}
+            onAdded={(err?: Error) => onAdded(err)}
+            onCancelled={() => setShowAdd(false)}
+          />
+        )}
+        {itemLoadError ? (
+          <div>Failed to load Items: {itemLoadError.message}</div>
+        ) : (
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Name</th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
-      )}
-    </div>
+            </thead>
+            <tbody>
+              {items!.map((i) => (
+                <tr key={i.id}>
+                  <td>
+                    <div className="ItemRowContainer">
+                      <div className="ItemName">
+                        <Link to={"items/" + i.id}>{i.name}</Link>
+                      </div>
+                      <Button variant="danger" onClick={() => deleteClicked(i)}>
+                        Delete
+                      </Button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
+      </div>
+      <Modal show={showDeleteConfirmation} onHide={deleteItemCancelled}>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete Item '{itemToDelete?.name}'</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Are you sure you want to delete the item named '{itemToDelete?.name}'
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={deleteItemCancelled}>
+            Cancel
+          </Button>
+          <Button variant="primary" onClick={deleteItemConfirmed}>
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
