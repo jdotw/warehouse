4b10f6e066cae8df47b6655275095094e8d10d3f
James Wilson <jw@jameswilson.io>
2022-11-27 21:55:42 +1100

    16.6k tps - unsafe and unchecked pool connection method

--- services/stock-rust-salvo/src/main.rs
+++ services/stock-rust-salvo/src/main.rs
@@ -4,6 +4,7 @@
 use self::models::*;
 use self::schema::categories::dsl as schema;
 use diesel::prelude::*;
+use diesel::r2d2::{ConnectionManager, Pool, PoolError, PooledConnection};
 use once_cell::sync::OnceCell;
 use salvo::prelude::*;
 use std::str::FromStr;
@@ -13,14 +14,17 @@ use uuid::Uuid;
 
 static POOL: OnceCell<PgPool> = OnceCell::new();
 
+fn connect() -> Result<PooledConnection<ConnectionManager<PgConnection>>, PoolError> {
+    unsafe { POOL.get_unchecked().get() }
+}
+
 #[handler]
 async fn get_categories(res: &mut Response) {
     let results = tokio::task::spawn_blocking(|| {
-        let pool = POOL.get().expect("pool is not initialized");
-        let connection = &mut pool.get().unwrap();
+        let mut connection = connect().unwrap();
         schema::categories
             .limit(5)
-            .load::<Category>(connection)
+            .load::<Category>(&mut connection)
             .expect("Error loading categories")
     })
     .await
@@ -31,26 +35,24 @@ async fn get_categories(res: &mut Response) {
 
 #[handler]
 async fn create_category(req: &mut Request, res: &mut Response) {
-    let pool = POOL.get().expect("pool is not initialized");
-    let connection = &mut pool.get().unwrap();
+    let mut connection = connect().unwrap();
     let category = req.parse_json::<NewCategory>().await.unwrap();
     let result = diesel::insert_into(schema::categories)
         .values(&category)
-        .get_result::<Category>(connection)
+        .get_result::<Category>(&mut connection)
         .expect("Error creating new category");
     res.render(Json(result));
 }
 
 #[handler]
 async fn update_category(req: &mut Request, res: &mut Response) {
-    let pool = POOL.get().expect("pool is not initialized");
-    let connection = &mut pool.get().unwrap();
+    let mut connection = connect().unwrap();
     let id = req.params().get("id").cloned().unwrap_or_default();
     let id = Uuid::from_str(&id).unwrap();
     let category = req.parse_json::<UpdateCategory>().await.unwrap();
     let result = diesel::update(schema::categories.find(id))
         .set(&category)
-        .get_result::<Category>(connection)
+        .get_result::<Category>(&mut connection)
         .expect("Error creating new category");
     res.render(Json(result));
 }
@@ -59,12 +61,11 @@ async fn update_category(req: &mut Request, res: &mut Response) {
 async fn get_category(req: &Request, res: &mut Response) {
     let requested_id = req.params().get("id").cloned().unwrap_or_default();
     let requested_id = Uuid::from_str(&requested_id).unwrap();
-    let pool = POOL.get().expect("pool is not initialized");
-    let connection = &mut pool.get().unwrap();
+    let mut connection = connect().unwrap();
     let results = schema::categories
         .filter(schema::id.eq(requested_id))
         .limit(1)
-        .load::<Category>(connection)
+        .load::<Category>(&mut connection)
         .expect("Error loading specific category");
     res.render(Json(results));
 }
