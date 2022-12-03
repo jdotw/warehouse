97c842da96389e580060e2cf088c0143e1eddf5b
James Wilson <jw@jameswilson.io>
2022-12-03 12:11:37 +1100

    - use multi-thread rt feature - separate function for synch vs blocking db calls

--- services/stock-rust-salvo/src/main.rs
+++ services/stock-rust-salvo/src/main.rs
@@ -6,6 +6,7 @@ extern crate diesel;
 mod models;
 mod schema;
 
+use anyhow::Error;
 use diesel::prelude::*;
 use diesel::r2d2::{ConnectionManager, Pool, PoolError, PooledConnection};
 use dotenvy::dotenv;
@@ -13,12 +14,15 @@ use models::*;
 use once_cell::sync::OnceCell;
 use salvo::prelude::*;
 use schema::categories::dsl::*;
+use tokio::runtime;
 
 use std::env;
 use std::str::FromStr;
 use std::sync::Arc;
 use std::thread::available_parallelism;
 use uuid::Uuid;
+
+mod server;
 
 static POOL: OnceCell<PgPool> = OnceCell::new();
 pub type PgPool = Pool<ConnectionManager<PgConnection>>;
@@ -39,7 +43,33 @@ fn build_pool(database_url: &str, size: u32) -> Result<PgPool, PoolError> {
 }
 
 #[handler]
-async fn get_categories(res: &mut Response) {
+async fn get_categories_canned(res: &mut Response) {
+    let results: [Category; 2] = [
+        Category {
+            id: Uuid::new_v4(),
+            name: String::new(),
+        },
+        Category {
+            id: Uuid::new_v4(),
+            name: String::new(),
+        },
+    ];
+    res.render(Json(results));
+}
+
+#[handler]
+async fn get_categories_synch(res: &mut Response) -> Result<(), Error> {
+    let mut connection = connect().unwrap();
+    let results = categories
+        .limit(5)
+        .load::<Category>(&mut connection)
+        .expect("Error loading categories");
+    res.render(Json(results));
+    Ok(())
+}
+
+#[handler]
+async fn get_categories_spawn_blocking(res: &mut Response) {
     let results = tokio::task::spawn_blocking(|| {
         let mut connection = connect().unwrap();
         categories
@@ -49,7 +79,6 @@ async fn get_categories(res: &mut Response) {
     })
     .await
     .unwrap();
-
     res.render(Json(results));
 }
 
@@ -90,24 +119,31 @@ async fn get_category(req: &Request, res: &mut Response) {
     res.render(Json(results));
 }
 
-#[tokio::main()]
-async fn main() {
-    dotenv().ok();
-
-    let router = Arc::new(
-        Router::new().push(
-            Router::with_path("categories")
-                .get(get_categories)
-                .post(create_category)
-                .push(
-                    Router::with_path("<id>")
-                        .get(get_category)
-                        .patch(update_category),
-                ),
-        ),
+async fn serve() {
+    let router = Router::new().push(
+        Router::with_path("categories")
+            .get(get_categories_synch)
+            .post(create_category)
+            .push(
+                Router::with_path("<id>")
+                    .get(get_category)
+                    .patch(update_category),
+            ),
     );
 
+    // Server::new(TcpListener::bind("0.0.0.0:7878"))
+    //     .serve(Service::new(router))
+    //     .await;
+
+    server::builder().serve(Service::new(router)).await.unwrap();
+}
+
+fn main() {
+    console_subscriber::init();
+    dotenv().ok();
+
     let size = available_parallelism().map(|n| n.get()).unwrap_or(16);
+
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     POOL.set(
         build_pool(&database_url, size as u32)
@@ -115,7 +151,19 @@ async fn main() {
     )
     .ok();
 
-    Server::new(TcpListener::bind("0.0.0.0:7878"))
-        .serve(Service::new(router))
-        .await;
+    println!("Cores: {size}");
+    // for _ in 1..size {
+    //     let rt = runtime::Runtime::new().unwrap();
+    //     rt.block_on(serve());
+    // }
+    // println!("Started http server: 127.0.0.1:7878");
+
+    let rt = runtime::Builder::new_multi_thread()
+        .worker_threads(10)
+        .max_blocking_threads(4096)
+        .enable_all()
+        .build()
+        .unwrap();
+
+    rt.block_on(serve());
 }
